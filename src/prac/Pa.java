f04a6e2e1f736291e21eb3146f80490d856d35a6
monishaaaa23 <shasmile.bala@gmail.com>
2019-06-13 02:05:25 -0400

    hi

---
+++ src/prac/Pa.java
@@ -0,0 +1,14 @@
+package prac;
+
+import java.util.ArrayList;
+
+public class Pa {
+	public static void main(String[] args) {
+		ArrayList li=new ArrayList();
+		li.add(10);
+		li.add("kshda");
+		li.add("true");
+		System.out.println(li);
+	}
+
+}
